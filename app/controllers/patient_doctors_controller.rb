a4d9c0b02e23f381ad535dd40ae0d57aa0bb6057
Will Dunlap <dunlapww@gmail.com>
2020-11-09 11:22:15 -0700

    add seeds

--- app/controllers/patient_doctors_controller.rb
+++ app/controllers/patient_doctors_controller.rb
@@ -2,6 +2,6 @@ class PatientDoctorsController < ApplicationController
 
   def destroy
     PatientDoctor.find_by(doctor_id: params[:doctor_id], patient_id: params[:id]).delete
-    redirect_to "doctors/doctors/#{params[:doctor_id]}"
+    redirect_to "doctors/#{params[:doctor_id]}"
   end
 end
